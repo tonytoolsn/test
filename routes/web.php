b2ced938b4d55d15f18cc4d1156db45295e0e1c9
gh_tony <web109btony712@gmail.com>
2021-04-15 15:17:48 +0800

    purchases show

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['namespace'=>'\App\Http\Controllers'], function () {
+Route::get('/purchases','PurchasesController@index');
+Route::post('/purchases','PurchasesController@purchases');
+});
